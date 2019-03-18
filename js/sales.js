4a0ff694f3baadb14ff46d17d4e7b59db8a5dc8d
Floyd Orr <floydcanhelp@gmail.com>
2019-03-18 15:49:33 -0500

    inital commit switching to branch

---
+++ js/sales.js
@@ -0,0 +1,26 @@
+var root = document.getElementById('root');
+
+
+var hours = ['6am', '7am','8am','9am','10am', '11am', '12pm', '1pm','2pm','3pm','4pm'];
+
+var pikePlace = {
+  locationName: 'Pike Place market',
+  minCustomersPerHour: 23,
+  maxCustomersPerHour: 23,
+  avgCookiesPerSale: 6.3,
+  customersEachHour: 65,
+  cookiesEachHour: [],
+  totalSailyCookies: 0,
+  calcCustomersEachHour: function() {
+
+  },
+  calcCooiesEachHour: function() {
+
+  },
+  render: function() {
+
+  }
+
+}
+
+pikePlace.render();
