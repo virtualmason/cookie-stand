e5847ffd7898f8d0e2d0c967036da9fdb7a9f6cf
Floyd Orr <floydcanhelp@gmail.com>
2019-03-19 22:23:14 -0500

    add table

--- js/sales.js
+++ js/sales.js
@@ -1,84 +1,74 @@
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+var table = document.createElement('table');
+var trHeader = document.createElement('tr');
 
+// Left Corner table cell needs to be blank
+var blank = document.createElement('th');
+trHeader.appendChild(blank);
 
-function pikePlace (locationName, minCustomersPerHour,maxCustomersPerHour,avgCookiesPerSale) {
-  this.locationName = locationName,
-  this.minCustomersPerHour = minCustomersPerHour,
-  this.maxCustomersPerHour = maxCustomersPerHour,
-  this.avgCookiesPerSale = avgCookiesPerSale,
-  this.customersEachHour = [],
-  this.cookiesEachHour = [],
-  this.totalDailyCookies = 0
+for (var i = 0; i < hours.length; i++) {
+  var listItem = document.createElement('th');
+  listItem.textContent = hours[i];
+  trHeader.appendChild(listItem);
 }
 
-pikePlace.prototype.calcCustomersEachHour = function() {
-  // console.log(hours, "hours global variable1 ")
-  // console.log(this.time[0], "time object ")
+table.appendChild(trHeader);
+document.body.appendChild(table);
 
+function CookieStore(locationName, minCustomersPerHour, maxCustomersPerHour, avgCookiesPerSale) {
+    this.locationName = locationName;
+    this.minCustomersPerHour = minCustomersPerHour;
+    this.maxCustomersPerHour = maxCustomersPerHour;
+    this.avgCookiesPerSale = avgCookiesPerSale;
+    this.customersEachHour = [];
+    this.cookiesEachHour = [];
+    this.totalDailyCookies = 0;
+}
 
-     for(var i = 0; i < hours.length; i++) {
-      this.customersEachHour.push(Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour));
-     }
-    }
+CookieStore.prototype.calcCustomersEachHour = function () {
+  for (var i = 0; i < hours.length; i++) {
+    this.customersEachHour.push(Math.floor(Math.random() * (this.maxCustomersPerHour - this.minCustomersPerHour + 1) + this.minCustomersPerHour));
+  }
+}
 
-pikePlace.prototype.calcCookiesEachHour = function() {
-    for(var i = 0; i < this.customersEachHour.length; i++) {
-      this.cookiesEachHour.push(Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale));
-    }
+CookieStore.prototype.calcCookiesEachHour = function () {
+  for (var i = 0; i < this.customersEachHour.length; i++) {
+    this.cookiesEachHour.push(Math.ceil(this.customersEachHour[i] * this.avgCookiesPerSale));
+  }
+}
+
+CookieStore.prototype.calcTotalCookies = function () {
+  for (var i = 0; i < this.cookiesEachHour.length; i++) {
+    this.totalDailyCookies += this.cookiesEachHour[i];
+  }
+}
+
+CookieStore.prototype.render = function () {
+  this.calcCustomersEachHour();
+  this.calcCookiesEachHour();
+  this.calcTotalCookies();
+
+  var tr = document.createElement('tr');
+  var th = document.createElement('th');
+  th.textContent = this.locationName;
+  tr.appendChild(th);
+
+  for (var i = 0; i < this.cookiesEachHour.length; i++) {
+    var td = document.createElement('td');
+    td.textContent = this.cookiesEachHour[i];
+    tr.appendChild(td);
   }
 
-pikePlace.prototype.calcTotalCookies = function() {
-      for(var i = 0; i < this.cookiesEachHour.length; i++){
-        this.totalDailyCookies += this.cookiesEachHour[i];
-      }
-      
-    }
-
-    pikePlace.prototype.table = function() {
-      var table = document.createElement("table");
-      table.setAttribute("id", "myTable");
-      document.body.appendChild(table);
-
-      var tr = document.createElement("tr");
-      tr.setAttribute("id", "myTr");
-
-      var th = document.createElement("th");
-      th.setAttribute('id', 'myTh');
-
-      document.getElementById("myTable").appendChild(tr);
-    
-      document.getElementById("myTr").appendChild(th);
-      var thText = document.createTextNode("myTh");
-      document.getElementById("myTh").appendChild(thText);
-      document.getElementById("myTable").appendChild(tr);
-      var td = document.createElement("td");
-      var cel = document.createTextNode("cell");
-      td.appendChild(cel);
-      document.getElementById("myTr").appendChild(td);
-    }
-
- pikePlace.prototype.render = function() { 
-      this.calcCustomersEachHour();
-      this.calcCookiesEachHour();
-      this.calcTotalCookies();
-      this.table(); 
-      
+  table.appendChild(tr);
 }
-  
-      // var header = document.createElement('h2')
-      // header.textContent = this.locationName;
-      // document.body.appendChild(header);
-      // var unorderedList = document.createElement('ul')
-      // for(var i = 0; i < hours.length; i++){
-      //   var listItem = document.createElement('li');
-      //   listItem.textContent = hours[i] + ': ' + this.cookiesEachHour[i];
-      //   unorderedList.appendChild(listItem);
-      // }
-      // var total = document.createElement('li');
-      // total.textContent = 'Total: ' + this.totalDailyCookies;
-      // unorderedList.appendChild(total);
-      // document.body.appendChild(unorderedList);
-    
 
-    var PikePlaceMarket = new pikePlace('Pike Place Market', 23, 65, 6.3)
-    PikePlaceMarket.render();
+
+var PikePlace = new CookieStore('Pike Place Market', 23, 65, 6.3);
+var SeaTacInternationalAiport = new CookieStore('SeaTac International Aiport', 23, 65, 6.3);
+var SeattleCenter = new CookieStore('Seattle Center', 23, 65, 6.3);
+var FloydsCookieStand = new CookieStore('Floyds Cookie Stand', 23, 65, 6.3);
+
+PikePlace.render();
+SeaTacInternationalAiport.render();
+SeattleCenter.render();
+FloydsCookieStand.render();
